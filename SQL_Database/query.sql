21998ac14b78e066f48b5960ab96abacfaa106fc
lychosand <quinn.stevenson@tuta.io>
2019-01-24 13:27:25 -0500

    Showing output of query

--- SQL_Database/query.sql
+++ SQL_Database/query.sql
@@ -16,3 +16,18 @@ SELECT animal_id,
 FROM   ANIMALS
        INNER JOIN PHYSICAL_ATTRIBUTES
                ON ANIMALS.animal_id = PHYSICAL_ATTRIBUTES.a_id;
+
+
+/*
+OUTPUT:
+  0|Buzz|Male|5|Dog|German Sheperd|Short Hair|Brown/Black
+  1|Spike|Male|9|Dog|Terrier|Long Hair|White
+  2|Harold|Male|2|Snake|Anaconda|Scales|Green
+  3|Stella|Female|3|Cat|Tabby|Long Hair|Ginger
+  4|Waltz|Male|1|Rat|Brown Rat|Short Hair|Brown
+  5|Daisy|Female|2|Dog|Dalmatian|Short Hair|White/Black
+  6|Spits|Male|12|Cats|Russian Blue|Short Hair|Grey
+  7|Luna|Female|8|Dog|Husky|Short Hair|White/Black
+  8|Bella|Female|4|Cat|Maine Coon|Long Hair|Brown/Grey
+  9|Molly|Female|2|Dog|Poodle|Long Hair|Ginger
+*/
